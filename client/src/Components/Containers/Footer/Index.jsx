5a102c27b3c3f567a7c02c1d9327c193cf3ae737
Thomas-Son <thomasson8131@gmail.com>
2023-12-14 16:36:58 +0100

    2nd commit

--- client/src/Components/Containers/Footer/Index.jsx
+++ client/src/Components/Containers/Footer/Index.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 import styles from "./Footer.module.css"
 
@@ -7,7 +10,10 @@ function Footer() {
         <footer>
             <div className={styles.footer}>
                 <p>Olympien</p>
-                <p>twitter facebook</p>
+                <div className={styles.link}>
+                    <FontAwesomeIcon icon={faTwitter} style={{ color: "#6bd3ff", }} />
+                    <FontAwesomeIcon icon={faFacebook} style={{ color: "#14346c", }} />
+                </div>                
                 <p>By Thomas Son - 2023</p>
             </div>
         </footer>
